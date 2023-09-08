d334c47783289db81b5fcfeaced9dcc2024048c4
Vinny Kristina Sihombing <vinny.kristina@moonlay.com>
2023-09-08 10:58:48 +0700

    read csv

--- dummyTest.js
+++ dummyTest.js
@@ -1,54 +1,36 @@
 import http from 'k6/http';
-// import encoding from 'k6/encoding';
 import { group } from 'k6';
+import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
+import { SharedArray } from 'k6/data';
+import encoding from 'k6/encoding';
 
-export const options = {
-    // vus: 1,
-    // duration: '2s',
-    stages: [{ duration: '1s', target: 10, },
-            { duration: '2s', target: 10, },
-            { duration: '3s', target: 10, },
-            { duration: '1s', target: 10, },
-            { duration: '1s', target: 10, },
-            { duration: '1s', target: 10, },
-            { duration: '1s', target: 10, }], 
-};
+const username = 'dbo-order-srv';
+const password = 'dbo-order-srv-passwd';
+
+const csvData = new SharedArray('another data name', function () {
+  // Load CSV file and parse it using Papa Parse
+  return papaparse.parse(open('./data.csv'), { header: true }).data;
+});
+console.log(csvData.length);
 
 export default async function () {
+  const credentials = `${username}:${password}`;
+  const encodedCredentials = encoding.b64encode(credentials);
+  for (let i = 0; i < csvData.length; i++) {
+    // const test = `https://moonlay-api.dbo.dev/order-srv/delivery-orders?id=${csvData[i].id}&id2=${csvData[i].id2}`
 
-    group('regress 1', function () {
-        const regress1 = http.request('GET', 'https://reqres.in/api/users?page=2', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        console.log('regress 1', regress1)
-    });
+    const test = `https://moonlay-api.dbo.dev/order-srv/delivery-orders?id=${csvData[i].id}`
+    console.log(test);
 
-     group('regress 2', function () {
-        const regress2 = http.request('GET', 'https://reqres.in/api/users/2',{
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-         console.log('egress 2', regress2)
-     });
-    
-    group('regress 3', function () {
-        const regress3 = http.request('GET', 'https://reqres.in/api/users?page=4', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        console.log('regress 3', regress3)
+    group('Get DO Detail By DO ID', function () {
+      const getDoDetailByDoId = http.request('GET', test, "", {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Basic ${encodedCredentials}`,
+        },
+      })
+      console.log('Get DO Detail By DO ID', getDoDetailByDoId)
+      console.log(getDoDetailByDoId.body)
     });
-
-    group('regress 4', function () {
-        const regress4 = http.request('GET', 'https://reqres.in/api/users?page=3', {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        console.log('regress 4', regress4)
-    });
+  }
 }
